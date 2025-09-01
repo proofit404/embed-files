1b78918a3f6d1c39b45ca83127d2fe648bf1be6c
Josiah Kaviani <proofit404@gmail.com>
2025-09-01 16:30:17 +0400

    feat: prompt template #5

--- src/embed_files/__init__.py
+++ src/embed_files/__init__.py
@@ -12,21 +12,26 @@ from llama_cpp import llama_log_callback
 from llama_cpp import llama_log_set
 
 
-@llama_log_callback  # type: ignore[misc]
-def _ignore_log(level: int, text: bytes, user_data: c_void_p) -> None:
-    pass
-
-
-llama_log_set(_ignore_log, c_void_p(0))
-
-
 @command()
-@argument("files", nargs=-1, type=ClickPath(path_type=Path, exists=True))
-@option("-m", "--model", required=True, type=ClickPath(path_type=Path, exists=True))
-def cli(files: list[Path], model: Path) -> None:
-    if not files:
-        echo("No files specified", err=True)
-        exit(1)
+@argument(
+    "files",
+    nargs=-1,
+    type=ClickPath(path_type=Path, exists=True, dir_okay=False),
+)
+@option(
+    "-m",
+    "--model",
+    required=True,
+    type=ClickPath(path_type=Path, exists=True, dir_okay=False),
+)
+@option(
+    "-t",
+    "--template",
+    required=True,
+)
+def cli(files: list[Path], model: Path, template: str) -> None:
+    _verify_files(files)
+    _verify_template(template)
 
     llama = Llama(
         model_path=str(model),
@@ -38,8 +43,28 @@ def cli(files: list[Path], model: Path) -> None:
     data = {}
 
     for path in files:
-        results = llama.create_embedding(f"clustering: {path.read_text()}")
+        results = llama.create_embedding(template.format(path.read_text()))
         assert len(results["data"]) == 1
         data[str(path)] = results["data"][0]["embedding"]
 
     echo(dumps(data, ensure_ascii=False))
+
+
+def _verify_files(files: list[Path]) -> None:
+    if not files:
+        echo("No files specified", err=True)
+        exit(1)
+
+
+def _verify_template(template: str) -> None:
+    if "{}" not in template or template.count("{") != 1 or template.count("}") != 1:
+        echo("Invalid template", err=True)
+        exit(1)
+
+
+@llama_log_callback  # type: ignore[misc]
+def _ignore_log(level: int, text: bytes, user_data: c_void_p) -> None:
+    pass
+
+
+llama_log_set(_ignore_log, c_void_p(0))
--- tests/test_embed_files.py
+++ tests/test_embed_files.py
@@ -6,16 +6,53 @@ from click.testing import CliRunner
 from embed_files import cli
 
 
-@pytest.mark.parametrize(
-    "option_name",
-    [pytest.param("-m", id="0"), pytest.param("--model", id="1")],
+model_argument = pytest.mark.parametrize(
+    "model_argument",
+    [
+        pytest.param("-m", id="0"),
+        pytest.param("--model", id="1"),
+    ],
 )
-def test_embed_files(option_name: str) -> None:
+
+
+template_argument = pytest.mark.parametrize(
+    "template_argument",
+    [
+        pytest.param("-t", id="0"),
+        pytest.param("--template", id="1"),
+    ],
+)
+
+
+model_path = pytest.mark.parametrize(
+    ("model_path", "template_value"),
+    [
+        pytest.param("vendor/mxbai-embed-xsmall-v1.gguf", "{}", id="0"),
+        pytest.param("vendor/nomic-embed-text-v1.5.gguf", "clustering: {}", id="1"),
+    ],
+)
+
+
+@model_argument
+@template_argument
+@model_path
+def test_embed_files(
+    model_argument: str,
+    model_path: str,
+    template_argument: str,
+    template_value: str,
+) -> None:
     runner = CliRunner()
 
     result = runner.invoke(
         cli,
-        [option_name, "vendor/nomic-embed-text-v1.5.gguf", "docs/installation.md"],
+        [
+            model_argument,
+            model_path,
+            template_argument,
+            template_value,
+            "docs/installation.md",
+        ],
     )
     assert result.exit_code == 0
 
@@ -27,10 +64,95 @@ def test_embed_files(option_name: str) -> None:
     assert all(isinstance(x, float) for x in vector)
 
 
-def test_no_model_error() -> None:
+# Files.
+
+
+@model_argument
+@template_argument
+@model_path
+def test_no_files_error(
+    model_argument: str,
+    model_path: str,
+    template_argument: str,
+    template_value: str,
+) -> None:
     runner = CliRunner()
 
-    result = runner.invoke(cli, ["docs/installation.md"])
+    result = runner.invoke(
+        cli,
+        [model_argument, model_path, template_argument, template_value],
+    )
+    assert result.exit_code == 1
+
+    assert result.output == "No files specified\n"
+
+
+@model_argument
+@template_argument
+@model_path
+def test_files_does_not_exists_error(
+    model_argument: str,
+    model_path: str,
+    template_argument: str,
+    template_value: str,
+) -> None:
+    runner = CliRunner()
+
+    result = runner.invoke(
+        cli,
+        [model_argument, model_path, template_argument, template_value, "haontsuh.md"],
+    )
+    assert result.exit_code == 2
+
+    expected = """Usage: cli [OPTIONS] [FILES]...
+Try 'cli --help' for help.
+
+Error: Invalid value for '[FILES]...': File 'haontsuh.md' does not exist.
+"""
+    assert result.output == expected
+
+
+@model_argument
+@template_argument
+@model_path
+def test_files_is_dir_error(
+    model_argument: str,
+    model_path: str,
+    template_argument: str,
+    template_value: str,
+) -> None:
+    runner = CliRunner()
+
+    result = runner.invoke(
+        cli,
+        [model_argument, model_path, template_argument, template_value, "docs"],
+    )
+    assert result.exit_code == 2
+
+    expected = """Usage: cli [OPTIONS] [FILES]...
+Try 'cli --help' for help.
+
+Error: Invalid value for '[FILES]...': File 'docs' is a directory.
+"""
+    assert result.output == expected
+
+
+# Model.
+
+
+@template_argument
+@model_path
+def test_no_model_error(
+    model_path: str,
+    template_argument: str,
+    template_value: str,
+) -> None:
+    runner = CliRunner()
+
+    result = runner.invoke(
+        cli,
+        [template_argument, template_value, "docs/installation.md"],
+    )
     assert result.exit_code == 2
 
     expected = """Usage: cli [OPTIONS] [FILES]...
@@ -41,38 +163,25 @@ Error: Missing option '-m' / '--model'.
     assert result.output == expected
 
 
-def test_model_does_not_exists_error() -> None:
-    runner = CliRunner()
-
-    result = runner.invoke(cli, ["-m", "nshuasno.gguf", "docs/installation.md"])
-    assert result.exit_code == 2
-
-    expected = """Usage: cli [OPTIONS] [FILES]...
-Try 'cli --help' for help.
-
-Error: Invalid value for '-m' / '--model': Path 'nshuasno.gguf' does not exist.
-"""
-    assert result.output == expected
-
-
-def test_no_files_error() -> None:
-    runner = CliRunner()
-
-    result = runner.invoke(cli, ["-m", "vendor/nomic-embed-text-v1.5.gguf"])
-    assert result.exit_code == 1
-
-    assert result.output == "No files specified\n"
-
-
-def test_files_does_not_exists_error() -> None:
+@model_argument
+@template_argument
+@model_path
+def test_model_does_not_exists_error(
+    model_argument: str,
+    model_path: str,
+    template_argument: str,
+    template_value: str,
+) -> None:
     runner = CliRunner()
 
     result = runner.invoke(
         cli,
         [
-            "--model",
-            "vendor/nomic-embed-text-v1.5.gguf",
-            "haontsuh.md",
+            model_argument,
+            "nshuasno.gguf",
+            template_argument,
+            template_value,
+            "docs/installation.md",
         ],
     )
     assert result.exit_code == 2
@@ -80,6 +189,95 @@ def test_files_does_not_exists_error() -> None:
     expected = """Usage: cli [OPTIONS] [FILES]...
 Try 'cli --help' for help.
 
-Error: Invalid value for '[FILES]...': Path 'haontsuh.md' does not exist.
+Error: Invalid value for '-m' / '--model': File 'nshuasno.gguf' does not exist.
 """
     assert result.output == expected
+
+
+@model_argument
+@template_argument
+@model_path
+def test_model_is_dir_error(
+    model_argument: str,
+    model_path: str,
+    template_argument: str,
+    template_value: str,
+) -> None:
+    runner = CliRunner()
+
+    result = runner.invoke(
+        cli,
+        [
+            model_argument,
+            "vendor",
+            template_argument,
+            template_value,
+            "docs/installation.md",
+        ],
+    )
+    assert result.exit_code == 2
+
+    expected = """Usage: cli [OPTIONS] [FILES]...
+Try 'cli --help' for help.
+
+Error: Invalid value for '-m' / '--model': File 'vendor' is a directory.
+"""
+    assert result.output == expected
+
+
+# Template.
+
+
+@model_argument
+@model_path
+def test_no_template_error(
+    model_argument: str,
+    model_path: str,
+    template_value: str,
+) -> None:
+    runner = CliRunner()
+
+    result = runner.invoke(cli, [model_argument, model_path, "docs/installation.md"])
+    assert result.exit_code == 2
+
+    expected = """Usage: cli [OPTIONS] [FILES]...
+Try 'cli --help' for help.
+
+Error: Missing option '-t' / '--template'.
+"""
+    assert result.output == expected
+
+
+@model_argument
+@template_argument
+@model_path
+@pytest.mark.parametrize(
+    "template_invalid",
+    [
+        pytest.param("foo", id="0"),  # No {content} placeholder.
+        pytest.param("{foo}", id="1"),  # Unknown placeholder.
+        pytest.param("foo {", id="2"),  # Invalid syntax: unclosed "{" char.
+    ],
+)
+def test_invalid_template(
+    model_argument: str,
+    model_path: str,
+    template_argument: str,
+    template_value: str,
+    template_invalid: str,
+) -> None:
+    runner = CliRunner()
+
+    result = runner.invoke(
+        cli,
+        [
+            model_argument,
+            model_path,
+            template_argument,
+            template_invalid,
+            "docs/installation.md",
+        ],
+    )
+    assert result.exit_code == 1
+
+    assert result.output == "Invalid template\n"
